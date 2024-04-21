9ac72fe186eabd575bc8b3be998c426a51820d45
5unidhi20 <sunidhiiit21@gmail.com>
2024-04-21 12:32:13 +0530

    first commit

---
+++ src/main/java/com/zoggy/response/PayResponse.java
@@ -0,0 +1,17 @@
+package com.zoggy.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PayResponse {
+
+    private String intentID;
+
+    private String clientSecret;
+}
